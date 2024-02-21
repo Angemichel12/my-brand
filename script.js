33d0fd531fc594ad92c9272d5f610b37ca55ca7c
Ange Michel <angemicheluwizeye@gmail.com>
2024-02-21 17:12:09 +0100

    admin-dashboard.html: work on blogs section, projects and message section

--- script.js
+++ script.js
@@ -26,3 +26,60 @@ toggle.onclick = function () {
   navigation.classList.toggle("active");
   main.classList.toggle("active");
 };
+let allbuttons = document.querySelectorAll(".navigation ul li");
+let alldivision = document.querySelectorAll(".allRequestDiv");
+
+// by default allproduct division must be displayed
+alldivision[0].style.display = "flex";
+
+for (let i = 0; i < allbuttons.length; i++) {
+  allbuttons[i].addEventListener("click", () => {
+    for (let k = 0; k < alldivision.length; k++) {
+      alldivision[k].style.display = "none"; // hide all divisions
+    }
+    let sectionId = allbuttons[i]
+      .querySelector("span.admin-title")
+      .innerText.toLowerCase();
+    let section = document.getElementById(sectionId);
+    if (section) {
+      section.style.display = "flex"; // show the corresponding division if it exists
+    }
+  });
+}
+
+// upload image
+const selectImage = document.querySelector(".select-image");
+const inputFile = document.querySelector("#file");
+const imgArea = document.querySelector(".img-area");
+
+selectImage.addEventListener("click", function () {
+  inputFile.click();
+});
+
+inputFile.addEventListener("change", function () {
+  const image = this.files[0];
+  if (image.size < 2000000) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const allImg = imgArea.querySelectorAll("img");
+      allImg.forEach((item) => item.remove());
+      const imgUrl = reader.result;
+      const img = document.createElement("img");
+      img.src = imgUrl;
+      imgArea.appendChild(img);
+      imgArea.classList.add("active");
+      imgArea.dataset.img = image.name;
+    };
+    reader.readAsDataURL(image);
+  } else {
+    alert("Image size more than 2MB");
+  }
+});
+
+const addBlogBtn = document.querySelector("#addblogbtn");
+const listtBlogs = document.querySelector(".listBlogs");
+const addBlog = document.querySelector(".addBlog");
+addBlogBtn.addEventListener("click", () => {
+  listtBlogs.style.display = "none";
+  addBlog.style.display = "block";
+});
