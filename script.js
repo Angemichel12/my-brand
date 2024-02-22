c709bf71da6f6e7ff5f66acd98eab6e10568a644
Ange Michel <angemicheluwizeye@gmail.com>
2024-02-22 15:23:07 +0100

    index.html: login direct to admin panel

--- script.js
+++ script.js
@@ -120,3 +120,21 @@ window.onclick = function (event) {
     hideModal();
   }
 };
+
+// loginFormValidation
+function validateForm() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (username.trim() == "") {
+    alert("Please enter your username");
+    return false;
+  }
+
+  if (password.trim() == "") {
+    alert("Please enter your password");
+    return false;
+  }
+
+  return true;
+}
